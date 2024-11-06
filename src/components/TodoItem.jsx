b6518debcdda51dd4a4fb0bb0d896180bc1ee511
Giovanny Vargas <yovaz05@gmail.com>
2024-11-05 19:49:26 -0500

    clase 05-11-2024

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,11 @@
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+/* eslint-disable react/prop-types */
+import style from "./TodoItem.module.css";
+const TodoItem = ({ todo }) => {
+  return (
+    <li className={style.list}>
+      <div className={style.listItem}>{todo.title}</div>
+    </li>
+  );
 };
 
 export default TodoItem;
